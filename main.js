868b82b1863e3c1724bc913a7c7c927cdd6fb589
propstm <propstm@gmail.com>
2019-05-19 20:40:39 -0400

    Updated query logic.  Loads detail content on click.

--- main.js
+++ main.js
@@ -7,13 +7,49 @@ function processData(response){
 
 
     for (var i in response.items){
-        var title = response.items[i].snippet.title;
-        var thumbnailUrl = response.items[i].snippet.thumbnails.default.url;
+        //console.log(response.items[i]);
+        console.log('video id:' + response.items[i].id.videoId);
+        var item = response.items[i]
+        var videoId = item.id.videoId;
+        var title = item.snippet.title;
+        var thumbnailUrl = item.snippet.thumbnails.default.url;
+        
 
-        root.insertAdjacentHTML('beforeEnd', `<li>Title: ${title} | Thumbnail URL: ${thumbnailUrl}</li>`);
+        root.insertAdjacentHTML('beforeEnd', `<li onclick="loadVideo('${videoId}')"><img src="${thumbnailUrl}" alt="${title}" />${title}</li>`);
     }
 
     root.insertAdjacentHTML('beforeEnd', `</ul>`);
+}
+
+function processDetailData(response){
+    for (var i in response.items){
+        var item = response.items[i];
+        var title = item.snippet.title;
+        var description = item.snippet.description;
+        var likes = item.statistics.likeCount;
+        var dislikes = item.statistics.dislikeCount;
+        var views = item.statistics.viewCount;
+        
+        console.log('likes:' + likes);
+        console.log('dislikes:' + dislikes);
+        console.log('views:' + views);
+
+        document.getElementById('videoTitle').innerHTML = title;
+        document.getElementById('videoDescription').innerHTML = description;
+        document.getElementById('views').innerHTML = views;
+        document.getElementById('likes').innerHTML = likes;
+        document.getElementById('dislikes').innerHTML = dislikes;
+    }
+}
+
+function loadVideo(id){
+    console.log('load video fired');
+    var videoFrame = document.getElementById('videoFrame');
+    videoFrame.src = `https://www.youtube.com/embed/${id}`;
+    fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=${apiKey}`)
+    .then((res) => res.json())
+    .then(processDetailData)
+    .catch(renderError);
 }
 
 const renderError = (err = 'Error processing request') => document
